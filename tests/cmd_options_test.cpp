030095c3ba4333b3939e1242ddb9cb871336d303
SerWike <fullname123@mail.ru>
2026-06-16 18:32:44 +0000

    Refactor tests for 'ProgramOptions'

--- tests/cmd_options_test.cpp
+++ tests/cmd_options_test.cpp
@@ -1,82 +1,104 @@
 #include "cmd_options.h"
 #include <gtest/gtest.h>
 
-TEST(ProgramOptions, CorrectChecksumTest) {
+TEST(ProgramOptions, ValidParamTest) {
     char *argv[] = {
-        "Reserved for executable file path", "--command", "checksum", "-i", "input.txt",
+        (char *)"Reserved for executable file path",
+        (char *)"--command",
+        (char *)"encrypt",
+        (char *)"-i",
+        (char *)"input.txt",
+        (char *)"-o",
+        (char *)"output.txt",
+        (char *)"-p",
+        (char *)"password",
     };
     int argc = sizeof(argv) / sizeof(argv[0]);
 
     CryptoGuard::ProgramOptions options = {};
-    options.Parse(argc, argv);
 
-    EXPECT_EQ(CryptoGuard::ProgramOptions::COMMAND_TYPE::CHECKSUM, options.GetCommand());
-    EXPECT_EQ("input.txt", options.GetInputFile());
-    EXPECT_TRUE(options.GetOutputFile().empty());
-    EXPECT_TRUE(options.GetPassword().empty());
-}
-
-TEST(ProgramOptions, CorrectEncryptTest) {
-    char *argv[] = {"Reserved for executable file path",
-                    "--command",
-                    "encrypt",
-                    "--input",
-                    "raw.txt",
-                    "-o",
-                    "encrypted.txt",
-                    "-p",
-                    "1234"};
-    int argc = sizeof(argv) / sizeof(argv[0]);
-
-    CryptoGuard::ProgramOptions options = {};
-    options.Parse(argc, argv);
-
+    EXPECT_NO_THROW(options.Parse(argc, argv));
     EXPECT_EQ(CryptoGuard::ProgramOptions::COMMAND_TYPE::ENCRYPT, options.GetCommand());
-    EXPECT_EQ("raw.txt", options.GetInputFile());
-    EXPECT_EQ("encrypted.txt", options.GetOutputFile());
-    EXPECT_EQ("1234", options.GetPassword());
-}
-
-TEST(ProgramOptions, CorrectDecryptTest) {
-    char *argv[] = {"Reserved for executable file path",
-                    "--command",
-                    "decrypt",
-                    "-i",
-                    "encrypted.txt",
-                    "--output",
-                    "decrypted.txt",
-                    "-p",
-                    "1234"};
-    int argc = sizeof(argv) / sizeof(argv[0]);
-
-    CryptoGuard::ProgramOptions options = {};
-    options.Parse(argc, argv);
-
-    EXPECT_EQ(CryptoGuard::ProgramOptions::COMMAND_TYPE::DECRYPT, options.GetCommand());
-    EXPECT_EQ("encrypted.txt", options.GetInputFile());
-    EXPECT_EQ("decrypted.txt", options.GetOutputFile());
-    EXPECT_EQ("1234", options.GetPassword());
-}
-
-TEST(ProgramOptions, InvalidCommandTest) {
-    char *argv[] = {"Reserved for executable file path",
-                    "--command",
-                    "invalid",
-                    "-i",
-                    "input.txt",
-                    "--output",
-                    "output.txt",
-                    "-p",
-                    "password"};
-    int argc = sizeof(argv) / sizeof(argv[0]);
-
-    CryptoGuard::ProgramOptions options = {};
-    options.Parse(argc, argv);
-
-    EXPECT_NE(CryptoGuard::ProgramOptions::COMMAND_TYPE::CHECKSUM, options.GetCommand());
-    EXPECT_NE(CryptoGuard::ProgramOptions::COMMAND_TYPE::ENCRYPT, options.GetCommand());
-    EXPECT_NE(CryptoGuard::ProgramOptions::COMMAND_TYPE::DECRYPT, options.GetCommand());
     EXPECT_EQ("input.txt", options.GetInputFile());
     EXPECT_EQ("output.txt", options.GetOutputFile());
     EXPECT_EQ("password", options.GetPassword());
+}
+
+TEST(ProgramOptions, ValidParamTestDifferentOrder) {
+    char *argv[] = {
+        (char *)"Reserved for executable file path",
+        (char *)"-o",
+        (char *)"output.txt",
+        (char *)"--command",
+        (char *)"decrypt",
+        (char *)"-p",
+        (char *)"password",
+        (char *)"-i",
+        (char *)"input.txt",
+    };
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    CryptoGuard::ProgramOptions options = {};
+
+    EXPECT_NO_THROW(options.Parse(argc, argv));
+    EXPECT_EQ(CryptoGuard::ProgramOptions::COMMAND_TYPE::DECRYPT, options.GetCommand());
+    EXPECT_EQ("input.txt", options.GetInputFile());
+    EXPECT_EQ("output.txt", options.GetOutputFile());
+    EXPECT_EQ("password", options.GetPassword());
+}
+
+TEST(ProgramOptions, EmptyParamWhenUseHelp) {
+    char *argv[] = {(char *)"Reserved for executable file path", (char *)"--help"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    CryptoGuard::ProgramOptions options = {};
+
+    testing::internal::CaptureStdout();
+    EXPECT_NO_THROW(options.Parse(argc, argv));
+    std::string output = testing::internal::GetCapturedStdout();
+
+    EXPECT_FALSE(output.empty());
+    EXPECT_EQ((CryptoGuard::ProgramOptions::COMMAND_TYPE)-1, options.GetCommand());
+    EXPECT_TRUE(options.GetInputFile().empty());
+    EXPECT_TRUE(options.GetInputFile().empty());
+    EXPECT_TRUE(options.GetPassword().empty());
+}
+
+TEST(ProgramOptions, InvalidCommand) {
+    char *argv[] = {(char *)"Reserved for executable file path", (char *)"--command", (char *)"invalid"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    CryptoGuard::ProgramOptions options = {};
+    EXPECT_THROW(options.Parse(argc, argv), std::runtime_error);
+
+    EXPECT_EQ((CryptoGuard::ProgramOptions::COMMAND_TYPE)-1, options.GetCommand());
+    EXPECT_TRUE(options.GetInputFile().empty());
+    EXPECT_TRUE(options.GetInputFile().empty());
+    EXPECT_TRUE(options.GetPassword().empty());
+}
+
+TEST(ProgramOptions, NoParamValue) {
+    char *argv[] = {(char *)"Reserved for executable file path", (char *)"--input"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    CryptoGuard::ProgramOptions options = {};
+    EXPECT_THROW(options.Parse(argc, argv), std::runtime_error);
+
+    EXPECT_EQ((CryptoGuard::ProgramOptions::COMMAND_TYPE)-1, options.GetCommand());
+    EXPECT_TRUE(options.GetInputFile().empty());
+    EXPECT_TRUE(options.GetInputFile().empty());
+    EXPECT_TRUE(options.GetPassword().empty());
+}
+
+TEST(ProgramOptions, NoParams) {
+    char *argv[] = {(char *)"Reserved for executable file path"};
+    int argc = sizeof(argv) / sizeof(argv[0]);
+
+    CryptoGuard::ProgramOptions options = {};
+    EXPECT_NO_THROW(options.Parse(argc, argv));
+
+    EXPECT_EQ((CryptoGuard::ProgramOptions::COMMAND_TYPE)-1, options.GetCommand());
+    EXPECT_TRUE(options.GetInputFile().empty());
+    EXPECT_TRUE(options.GetInputFile().empty());
+    EXPECT_TRUE(options.GetPassword().empty());
 }
